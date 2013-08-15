5576f96692147b5a076c2481ba5fccda3f5675fb
Johannes Moersch <Johannes.Moersch@gmail.com>
2013-08-15 16:17:42 -0600

    Adds the QuickConverter markup extension.

--- QuickConverter.cs
+++ QuickConverter.cs
@@ -2,25 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Data;
 using System.Windows.Markup;
 
 namespace QuickConverter
 {
-	/// <summary>
-	/// This type can be substituted for System.Windows.Data.Binding. Both Convert and ConvertBack to be specified inline which makes two way binding possible.
-	/// </summary>
-	public class Binding : MarkupExtension
+	public class QuickConverter : MarkupExtension
 	{
 		private static Dictionary<string, Tuple<Func<object, object[], object>, string[]>> toFunctions = new Dictionary<string, Tuple<Func<object, object[], object>, string[]>>();
 		private static Dictionary<string, Tuple<Func<object, object[], object>, string[]>> fromFunctions = new Dictionary<string, Tuple<Func<object, object[], object>, string[]>>();
-
-		/// <summary>Creates a bound parameter. This can be accessed inside the converter as $P.</summary>
-		public System.Windows.Data.Binding P { get; set; }
 
 		/// <summary>Creates a constant parameter. This can be accessed inside the converter as $V0.</summary>
 		public object V0 { get; set; }
@@ -57,21 +47,62 @@ namespace QuickConverter
 		/// <summary>
 		/// This specifies the context to use for dynamic call sites.
 		/// </summary>
-		public Type DynamicContext { get; set; }
+		public Type DynamicContext { get; set; }	
 
-		public Binding()
+		public QuickConverter()
 		{
 		}
 
-		public Binding(string convert)
+		public QuickConverter(string convert)
 		{
 			Convert = convert;
+		}
+
+		internal static Tuple<Delegate, ParameterExpression[]> GetLambda(string expression, bool convertBack, Type dynamicContext)
+		{
+			List<ParameterExpression> parameters = new List<ParameterExpression>();
+			Expression exp = EquationTokenizer.Tokenize(expression).GetExpression(parameters, dynamicContext);
+			ParameterExpression invalid;
+			if (convertBack)
+				invalid = parameters.FirstOrDefault(par => !((par.Name[0] == 'V' && par.Name.Length == 2 && Char.IsDigit(par.Name[1])) || (par.Name == "value")));
+			else
+				invalid = parameters.FirstOrDefault(par => !((par.Name[0] == 'P' && par.Name.Length == 1) || (par.Name[0] == 'V' && par.Name.Length == 2 && Char.IsDigit(par.Name[1]))));
+			if (invalid != null)
+				throw new Exception("\"$" + invalid.Name + "\" is not a valid parameter name for conversion " + (convertBack ? "to" : "from") + " source.");
+			Delegate del = Expression.Lambda(Expression.Convert(exp, typeof(object)), parameters.ToArray()).Compile();
+			return new Tuple<Delegate, ParameterExpression[]>(del, parameters.ToArray());
+		}
+
+		internal static Expression GetFinishedLambda(Tuple<Delegate, ParameterExpression[]> lambda, out ParameterExpression inputP, out ParameterExpression inputV, out ParameterExpression value, out List<string> values)
+		{
+			List<string> vals = new List<string>();
+			int vCount = 0;
+			ParameterExpression val = Expression.Parameter(typeof(object));
+			ParameterExpression inP = Expression.Parameter(typeof(object));
+			ParameterExpression inV = Expression.Parameter(typeof(object[]));
+			var arguments = lambda.Item2.Select<ParameterExpression, Expression>(par =>
+			{
+				if (par.Name[0] == 'P')
+					return inP;
+				else if (par.Name[0] == 'V')
+				{
+					vals.Add(par.Name);
+					return Expression.ArrayIndex(inV, Expression.Constant(vCount++));
+				}
+				return val;
+			});
+			inputP = inP;
+			inputV = inV;
+			value = val;
+			values = vals;
+
+			return Expression.Call(Expression.Constant(lambda.Item1, lambda.Item1.GetType()), lambda.Item1.GetType().GetMethod("Invoke"), arguments);
 		}
 
 		public override object ProvideValue(IServiceProvider serviceProvider)
 		{
 			var pvt = serviceProvider as IProvideValueTarget;
-			if (pvt == null || P == null)
+			if (pvt == null)
 				return null;
 
 			ParameterExpression inputP, inputV, value;
@@ -111,11 +142,7 @@ namespace QuickConverter
 			if (fromFunc != null)
 				fromVals = fromFunc.Item2.Select(str => typeof(Binding).GetProperty(str).GetValue(this, null)).ToArray();
 
-			var holder = new System.Windows.Data.MultiBinding() { Mode = P.Mode, UpdateSourceTrigger = P.UpdateSourceTrigger };
-			holder.Bindings.Add(P);
-			holder.Converter = new DynamicConverter(toFunc != null ? toFunc.Item1 : null, fromFunc != null ? fromFunc.Item1 : null, toVals, fromVals);
-
-			return holder.ProvideValue(serviceProvider);
+			return new DynamicSingleConverter(toFunc != null ? toFunc.Item1 : null, fromFunc != null ? fromFunc.Item1 : null, toVals, fromVals);
 		}
 	}
 }
